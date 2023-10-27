a12b1a5aefbc1dc2ab3aa44509255a12c5bf0c24
Your Name <you@example.com>
2023-10-26 21:13:57 -0300

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,35 @@ function App() {
 
   const stages = [
     {id:1, name:'start'},
-    {id:2, name:'part1'},
-    {id:3, name:'part2'},
+    {id:2, name:'part1', pergunta:'nome da denise?',resp1:'nery', resp2:'silva', resp3:'string', resp4:'martins'},
+    {id:3, name:'part2',  pergunta:'lucas da o cu?',resp1:'ss', resp2:'nn', resp3:'talvez', resp4:'sipa'},
   ]
 
   const [tries,setTries] = useState('0')
-  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [gameStage, setGameStage] = useState(0)
+  const[pergunta,setPergunta]= useState()
+  const [resp1,setResp1] = useState()
+  const [resp2,setResp2] = useState()
+  const [resp3,setResp3] = useState()
+  const [resp4,setResp4] = useState()
+  const [n,setN] = useState(0)
 
-  const startGame = () => {
-    setGameStage(stages[1].name)
+  const passFase = () => {
+    setN(gameStage + 1)
+    setGameStage(n)
+   console.log(n)
   }
 
   const mistake = () => {
     setGameStage(stages[0].name)
   }
 
+
   return (
     <div className="App">
-      {gameStage === 'start' ? <Start tries={tries} startGame={startGame}/> : null}
-      {gameStage === 'part1' ? <First1 pergunta="Nome da denise?" resp1="nery" resp2="silva" resp3="string" resp4="martins"/> : null}
-      {gameStage === 'part2' ? <First1 pergunta="Penis?" resp1="sim" resp2="s33333im" resp3="s17im" resp4="s22im"/> : null}
+      {gameStage === 0 ? <Start tries={tries} startGame={passFase}/> : <First1 pergunta="Nome da denise?" resp1="nery" resp2="silva" resp3="string" resp4="martins"/>}
+      
+     
     </div>
   )
 }
--- src/components/First1.jsx
+++ src/components/First1.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import './First1.css'
-function First1(props) {
+function First1({pergunta,resp1,resp2,resp3,resp4,startGame}) {
   return (
     <div className="First1">
 
-    <h1>{props.pergunta}</h1>
+    <h1>{pergunta}</h1>
 
     <div id="row1" className='row'>
-    <div className="box">{props.resp1}</div>
-    <div className="box">{props.resp2}</div>
+    <div className="box" onClick={startGame}>{resp1}</div>
+    <div className="box">{resp2}</div>
     </div>
     
     <div id="row2" className='row'>
-    <div className="box">{props.resp3}</div>
-    <div className="box">{props.resp4}</div>
+    <div className="box">{resp3}</div>
+    <div className="box">{resp4}</div>
     </div>
     </div>
   )
