3a70ea5a660c161575ae4359fd5257a79b511f6b
Vitor Colombiano <vitor.colombiano@thinklife.com.br>
2023-12-01 00:31:56 -0300

    edicao css

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './Error.css' // Certifique-se de importar o arquivo CSS
 
-function Error({text, reboot}) {
-  //text explica o erro
-  //reboot volta para o início e adiciona 1 na contagem
+function Error({ text, reboot }) {
+  // text explica o erro
+  // reboot volta para o início e adiciona 1 na contagem
   return (
     <div className='Error'>
-    <div>{text}</div>
-    <button onClick={reboot}>voltar</button> 
+      <div className='error-text'>{text}</div>
+      <button onClick={reboot}>Voltar</button>
     </div>
-  )
+  );
 }
 
-export default Error
+export default Error;
--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,14 +1,22 @@
-import React from 'react'
-import './Start.css'
+import React from 'react';
+import logo from '../image/Unifesp_simples_policromia_RGB.png'; // Caminho relativo à localização de Start.jsx
+import './Start.css'; // Certifique-se de importar o arquivo CSS
 
-function Start({tries, pathing}) {
+function Start({ tries, pathing }) {
+  const startStyle = {
+    backgroundColor: '#3498db', // Defina a cor de fundo desejada
+  };
+
   return (
     <div className="Start">
-    <h1> AOC QUIZZ </h1>
+      {/* Adicione a imagem do logo acima do texto "AOC QUIZZ" */}
+      <img src={logo} alt="Logo" className="logo" />
+
+      <h1> AOC QUIZZ </h1>
       <button onClick={pathing}>Start Game</button>
       <p> Quantidades de tentativas: {tries}</p>
-      </div>
-  )
+    </div>
+  );
 }
 
-export default Start
+export default Start;
